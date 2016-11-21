b7d1e92cb2b75ac4a44fde0721bc39b5ea8506a1
felix <firomerorom4@gmail.com>
2016-11-21 10:48:58 -0500

    Actualizando la nomenclatura de los registros

--- js/app.controllers.js
+++ js/app.controllers.js
@@ -92,26 +92,66 @@ angular.module('app.router').controller('scotchController',function ($scope,Mana
 
 
 })
-.controller("homeController",function ($scope, Manager,Provincias,OSDE,Ministerio,Sectores,Sustancias) {
+.controller("generalController",function ($scope, Manager,Provincias,OSDE,Ministerio,Sectores,Sustancias,AlternativaHFC,AlternativaHFCMezclas,AlternativaHFO,AlternativaOtras,SectoresAnexo,RA,Util) {
+
+//DB en memoria
+$scope.documents = [];
+//Informacion general
 $scope.Provincias = Provincias;
 $scope.OSDE = OSDE;
 $scope.Ministerio = Ministerio;
-$scope.Sectores = Sectores.map(function (el) {
-    el.value = false;
-    return el;
-});
-$scope.Sustancias = Sustancias;
+
 $scope.general = {
     "provincia":Provincias[0],
     "ministerio":Ministerio[0],
     "osde":OSDE[0],
     "empresa":"",
-    "sustancia":Sustancias[0],
-    "sectores":[],
     "tipo":"general"
-};
-$scope.registros = [];
-    $scope.documents = [];
+        };
+
+
+
+// Uso general alternativas a las SAO en la actualidad
+
+$scope.Sectores = Sectores.map(function (el) {
+    el.value = false;
+    return el;
+});
+$scope.Sustancias = Sustancias;
+
+$scope.general1 = {
+            "sustancia":Sustancias[0],
+            "sectores":[],
+            "tipo":"general1"
+        };
+
+
+
+//    Resumen de su uso en todos los sectores para cada año entre 2011-2015
+
+$scope.AlternativaHFC = AlternativaHFC;
+$scope.AlternativaHFCMezclas = AlternativaHFCMezclas;
+$scope.AlternativaHFO = AlternativaHFO;
+$scope.AlternativaOtras = AlternativaOtras;
+$scope.RA = RA;
+$scope.SectoresAnexo = SectoresAnexo.map(function (el) {
+       el.value = false;
+       return el;
+});
+$scope.general2 = {
+            "otroHFC":"",
+            "otroHFCMezclas":"",
+            "otroHFO":"",
+            "otroAlternativasOtras":"",
+            "alternativaHFC":AlternativaHFC[0],
+            "alternativaHFCMezclas":AlternativaHFCMezclas[0],
+            "alternativaHFO":AlternativaHFO[0],
+            "alternativaOtras":AlternativaOtras[0],
+            "ra":RA[0],
+            "sectoresAnexo":[],
+            "tipo":"general2"
+        };
+
 
     ////LOCAL MEMBERS
     function init() {
@@ -125,7 +165,7 @@ $scope.registros = [];
     function AddElement(element) {
         Manager.create(element).then(function (result) {
             //todo on success
-            $scope.registros.push(element);
+            $scope.documents.push(element);
             console.info(JSON.stringify(result));
         }).catch(function (reason) {
             //todo on fail
@@ -136,9 +176,23 @@ $scope.registros = [];
     ///SCOPE MEMBERS
     $scope.Add = function (element) {
         //todo validar datos
-        element.sectores = element.sectores.concat($scope.Sectores.filter(function (el) {
-            return el.value==true;
-        }));
+        element = Util.collect($scope.general,element);
+        
+        switch (element.tipo){
+            case 'general1':
+                element.sectores = element.sectores.concat($scope.Sectores.filter(function (el) {
+                    return el.value==true;
+                }));
+                break;
+            case 'general2':
+                element.sectoresAnexo = element.sectoresAnexo.concat($scope.SectoresAnexo.filter(function (el) {
+                    return el.value==true;
+                }));
+                break;
+            default:
+
+                break;
+        }
 
         AddElement(element);
     };
